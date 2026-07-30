83851aa794a627e74fd31a23e1f5daa51052f2e4
unknown <andreiacojido@gmail.com>
2026-07-30 12:12:19 +0800

    added form, library display, and new book addition to library array

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
-console.log("test");
+const myLibrary = [];
+
+
+function book (title, author, release, haveRead){
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.release = release;
+    this.haveRead = haveRead;
+}
+
+function addBookToLibrary (title, author, release, haveRead){
+    const newbook = new book (title, author, release, haveRead);
+    myLibrary.push(newbook);
+}
+
+addBookToLibrary("Harry Potter" , "J.K Rowling" , 1990, "haveRead");
+addBookToLibrary("Romeo & Juliet" , "Sheakspeare", 1980, "havRead");
+console.log(myLibrary);
+
+
+
+const container = document.querySelector("#container");
+
+function displayBooks (){
+        container.innerHTML = "";
+
+    myLibrary.forEach((book)=> {
+        let card = document.createElement("div");
+        card.classList.add("card");
+
+        let title = document.createElement("p");
+        let author = document.createElement("p");
+         let release = document.createElement("p");
+          let haveRead = document.createElement("p");
+
+          title.textContent = book.title;
+          author.textContent = book.author;
+          release.textContent = book.release;
+          haveRead.textContent = book.haveRead;
+
+          card.append(title, author ,release,haveRead);
+          container.append(card);
+    })
+}
+
+
+//display call 
+displayBooks();
+
+// new book function
+
+let newBook = document.querySelector("#newBook-btn");
+let form = document.querySelector("#newBook-form");
+
+newBook.addEventListener("click", ()=> {
+    form.classList.remove("hidden");
+})
+
+//submit and add to library
+
+let submitBtn = document.querySelector("#submit-btn");
+
+submitBtn.addEventListener("click" , (e)=> {
+    e.preventDefault();
+
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let release = document.querySelector("#release").value;
+    let haveRead = document.querySelector("#haveRead").value;
+
+    addBookToLibrary(title, author, release, haveRead);
+    displayBooks();
+
+    //hide form again
+
+    form.classList.add("hidden");
+
+
+})
